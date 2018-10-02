13eeef82655e5f53c7e194d572fe845cecc78b07
Lunerr <kyleroe94@gmail.com>
2018-10-02 02:02:25 -0500

    fix

--- src/commands/polls/Poll.js
+++ src/commands/polls/Poll.js
@@ -35,7 +35,7 @@ class Poll extends patron.Command {
     }
 
     for (const key in args.poll.choices) {
-      choices += choiceNum++ + key + ': ' + args.poll.choices[key] + ',\n';
+      choices += choiceNum++ + '. ' + key + ': ' + args.poll.choices[key] + ',\n';
     }
 
     return msg.channel.createMessage('**Index:** ' + args.poll.index + '\n**Creator:** ' + creator + '\n**Answers:** \n' + choices.substring(0, choices.length - 2) + '\n**Ending:** ' + timeLeft.days + ' days, ' + timeLeft.hours + ' hours, ' + timeLeft.minutes + ' minutes, ' + timeLeft.seconds + ' seconds' + '\n**Elder Only:** ' + (args.poll.elderOnly === true ? 'Yes' : 'No') + '\n**Mod Only:** ' + (args.poll.modOnly === true ? 'Yes' : 'No'), { title: args.poll.name });

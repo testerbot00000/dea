f4a580d215e10da80590317b66c5347db3512efb
Lunerr <kyleroe94@gmail.com>
2018-10-02 01:14:29 -0500

    io

--- src/events/message.js
+++ src/events/message.js
@@ -67,12 +67,12 @@ client.on('message', (msg) => {
           break;
       }
 
-      await Logger.log('Unsuccessful command result: ' + msg.id + ' User: ' + msg.author.tag + ' User ID: + ' + msg.author.id + ' Guild: ' + msg.guild.name + ' Channel: ' + msg.channel.name + ' Content ' + msg.content + ' | Reason: ' + result.errorReason, 'DEBUG');
+      await Logger.log('Unsuccessful command result: ' + msg.id + ' User: ' + msg.author.tag + ' User ID: ' + msg.author.id + ' Guild: ' + msg.guild.name + ' Channel: ' + msg.channel.name + ' Content ' + msg.content + ' | Reason: ' + result.errorReason, 'DEBUG');
 
       return msg.tryCreateErrorReply(message);
     }
 
-    return Logger.log('Successful command result: ' + msg.id + ' User: ' + msg.author.tag + ' User ID: + ' + msg.author.id + ' Guild: ' + msg.guild.name + ' Channel: ' + msg.channel.name + ' Content ' + msg.content, 'DEBUG');
+    return Logger.log('Successful command result: ' + msg.id + ' User: ' + msg.author.tag + ' User ID: ' + msg.author.id + ' Guild: ' + msg.guild.name + ' Channel: ' + msg.channel.name + ' Content ' + msg.content, 'DEBUG');
   })()
     .catch((err) => Logger.handleError(err));
 });

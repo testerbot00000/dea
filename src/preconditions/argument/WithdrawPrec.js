eebac2ccbb0471fb5c6c8d190b2ade2d144a1de3
vim2meta <vim2meta@tutanota.com>
2018-10-07 18:41:00 -0400

    diff withdraw caps for gang ranks (#6)
    saucy

--- src/preconditions/argument/WithdrawPrec.js
+++ src/preconditions/argument/WithdrawPrec.js
@@ -10,12 +10,14 @@ class WithdrawPrec extends patron.ArgumentPrecondition {
 
   async run(command, msg, argument, args, value) {
     const gang = await msg.client.db.gangRepo.findOne({ $or: [{ members: msg.author.id }, { elders: msg.author.id }, { leaderId: msg.author.id }], $and: [{ guildId: msg.guild.id }] });
+    const maxWithdraw = gang.leaderId == msg.author.id || gang.elders.contains(msg.author.id) ? 0.2 : 0.05;
+    const maxVal = Math.round(NumberUtil.realValue(gang.wealth) * maxWithdraw, 2);
 
-    if (value <= Math.round(NumberUtil.realValue(gang.wealth) * 0.2, 2)) {
+    if (value <= maxVal) {
       return patron.PreconditionResult.fromSuccess();
     }
 
-    return patron.PreconditionResult.fromError(command, 'You may only withdraw 20% of your gang\'s wealth, that is ' + (NumberUtil.realValue(gang.wealth) * 0.2).USD() + '.');
+    return patron.PreconditionResult.fromError(command, 'You may only withdraw ' + maxWithdraw.toLocaleString('en', { style: 'percent' }) + ' of your gang\'s wealth, that is ' + maxVal.USD() + '.');
   }
 }
 

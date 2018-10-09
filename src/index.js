7d3b05547488c60da3e386f48cede737f5192436
Ash <28841569+Asshley@users.noreply.github.com>
2018-10-09 19:58:25 -0400

    this is so sad
    can we get 5 likes

--- src/index.js
+++ src/index.js
@@ -10,15 +10,9 @@ const Logger = require('./utility/Logger.js');
 
   await patron.RequireAll(path.join(__dirname, 'events'));
   await IntervalService.initiate(client);
-
-  await client.db.connect('mongodb://deabeta:deabeta1@ds239681.mlab.com:39681/deabeta');
-  await client.login('NDk4OTY3Mjk2NDg0ODM1MzM4.Dp62Yg.Jw0R_NPpLzBMFH6aPbF86ly329o');
-
-  //await client.db.connect('mongodb://dea:deadea1@ds123603.mlab.com:23603/rewrite');
-  //await client.login('MzQwNjE0NDc3ODc3NjA4NDU4.Dpup0w.NHTPCAr_a2pzCDEGOyT2u8eDSy0');
-
-  //await client.db.connect(process.env.MONGO_DB_URL);
-  //await client.login(process.env.BOT_TOKEN);
+  
+  await client.db.connect(process.env.MONGO_DB_URL);
+  await client.login(process.env.BOT_TOKEN);
 })()
   .catch(e => Logger.handleError(e));
 
